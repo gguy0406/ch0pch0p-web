0b05c46b70a7937488c29e9d10db658009ff4449
Trung D. Ngo <gguy0406@gmail.com>
2023-12-09 09:43:43 +0000

    Add Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: [],
+  templateUrl: "./app.component.html",
 })
 export class AppComponent {
-  title = 'ch0pch0p-web';
+  title = "ch0pch0p-web";
 }
