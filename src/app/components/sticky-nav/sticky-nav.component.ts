2d4a6dc2fffa0c9a6bbbe5100ac811262fc7202a
Trung D. Ngo <gguy0406@gmail.com>
2024-01-27 17:42:00 +0000

    chore: remove unnecessary ng zone

--- src/app/components/sticky-nav/sticky-nav.component.ts
+++ src/app/components/sticky-nav/sticky-nav.component.ts
@@ -1,4 +1,15 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, NgZone } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostBinding,
+  OnDestroy,
+  Signal,
+  WritableSignal,
+  computed,
+  signal,
+} from '@angular/core';
 
 import { NavComponent } from '../nav/nav.component';
 
@@ -10,22 +21,29 @@ import { NavComponent } from '../nav/nav.component';
   standalone: true,
   imports: [NavComponent],
 })
-export class StickyNavComponent implements AfterViewInit {
-  private _lastScrollTopValue: number = 0;
+export class StickyNavComponent implements AfterViewInit, OnDestroy {
+  @HostBinding('style.top.px') get topPosition() {
+    return this._topPosition();
+  }
 
-  constructor(
-    private _elementRef: ElementRef<HTMLElement>,
-    private _ngZone: NgZone
-  ) {}
+  private _lastScrollTopValue: number = 0;
+  private _isScrollingUp: WritableSignal<boolean> = signal(false);
+  private _topPosition: Signal<string> = computed(() =>
+    this._isScrollingUp() ? '0' : `-${this._elementRef.nativeElement.clientHeight}`
+  );
+  private _handleScrollEvent = (event: HTMLElementEventMap['scroll']) => {
+    const scrollTop = (event.target as HTMLDivElement).scrollTop;
+    this._isScrollingUp.set(scrollTop < this._lastScrollTopValue);
+    this._lastScrollTopValue = scrollTop;
+  };
+
+  constructor(private _elementRef: ElementRef<HTMLElement>) {}
 
   ngAfterViewInit(): void {
-    this._ngZone.runOutsideAngular(() => {
-      this._elementRef.nativeElement.parentElement?.addEventListener('scroll', (event) => {
-        const scrollTop = (event.target as HTMLDivElement).scrollTop;
-        this._elementRef.nativeElement.style.top =
-          scrollTop < this._lastScrollTopValue ? '0' : `-${this._elementRef.nativeElement.clientHeight}px`;
-        this._lastScrollTopValue = scrollTop;
-      });
-    });
+    this._elementRef.nativeElement.parentElement?.addEventListener('scroll', this._handleScrollEvent);
+  }
+
+  ngOnDestroy(): void {
+    this._elementRef.nativeElement.parentElement?.removeEventListener('scroll', this._handleScrollEvent);
   }
 }
--- src/app/directives/drag-to-scroll.ts
+++ src/app/directives/drag-to-scroll.ts
@@ -1,49 +1,56 @@
-import { Directive, ElementRef, NgZone } from '@angular/core';
+import { Directive, ElementRef, HostBinding, OnDestroy, OnInit, WritableSignal, signal } from '@angular/core';
 
 @Directive({
   selector: '[appDragToScroll]',
   standalone: true,
 })
-export class DragToScrollDirective {
-  private _nativeElement: HTMLElement;
+export class DragToScrollDirective implements OnInit, OnDestroy {
+  @HostBinding('style.cursor') get cursorStyle() {
+    return this._cursorStyle();
+  }
+  @HostBinding('class') get classes() {
+    return 'select-none pointer-events-none-container';
+  }
+
   private _pos = { top: 0, left: 0, x: 0, y: 0 };
-
-  constructor(_elementRef: ElementRef, ngZone: NgZone) {
-    this._nativeElement = _elementRef.nativeElement;
-    this._nativeElement.style.cursor = 'grab';
-    this._nativeElement.style.userSelect = 'none';
-    this._nativeElement.style.webkitUserSelect = 'none';
-    this._nativeElement.classList.add('pointer-events-none-container');
-
-    ngZone.runOutsideAngular(() => {
-      const handleMouseMove = (event: MouseEvent) => {
-        const dx = event.clientX - this._pos.x;
-        const dy = event.clientY - this._pos.y;
-
-        this._nativeElement.scrollTop = this._pos.top - dy;
-        this._nativeElement.scrollLeft = this._pos.left - dx;
-      };
-
-      const handleMouseUpOrLeave = () => {
-        this._nativeElement.style.cursor = 'grab';
-        this._nativeElement.removeEventListener('mousemove', handleMouseMove);
-        this._nativeElement.removeEventListener('mouseup', handleMouseUpOrLeave);
-        this._nativeElement.removeEventListener('mouseleave', handleMouseUpOrLeave);
-      };
-
-      this._nativeElement.addEventListener('mousedown', (event: MouseEvent) => {
-        Object.assign(this._pos, {
-          left: this._nativeElement.scrollLeft,
-          top: this._nativeElement.scrollTop,
-          x: event.clientX,
-          y: event.clientY,
-        });
-
-        this._nativeElement.style.cursor = 'grabbing';
-        this._nativeElement.addEventListener('mousemove', handleMouseMove);
-        this._nativeElement.addEventListener('mouseup', handleMouseUpOrLeave);
-        this._nativeElement.addEventListener('mouseleave', handleMouseUpOrLeave);
-      });
+  private _nativeElement: HTMLElement;
+  private _cursorStyle: WritableSignal<string> = signal('grab');
+  private _handleMouseDown = (event: MouseEvent) => {
+    Object.assign(this._pos, {
+      left: this._nativeElement.scrollLeft,
+      top: this._nativeElement.scrollTop,
+      x: event.clientX,
+      y: event.clientY,
     });
+
+    this._cursorStyle.set('grabbing');
+    this._nativeElement.addEventListener('mousemove', this._handleMouseMove);
+    this._nativeElement.addEventListener('mouseup', this._handleMouseUpOrLeave);
+    this._nativeElement.addEventListener('mouseleave', this._handleMouseUpOrLeave);
+  };
+  private _handleMouseMove = (event: MouseEvent) => {
+    const dx = event.clientX - this._pos.x;
+    const dy = event.clientY - this._pos.y;
+
+    this._nativeElement.scrollTop = this._pos.top - dy;
+    this._nativeElement.scrollLeft = this._pos.left - dx;
+  };
+  private _handleMouseUpOrLeave = () => {
+    this._cursorStyle.set('grab');
+    this._nativeElement.removeEventListener('mousemove', this._handleMouseMove);
+    this._nativeElement.removeEventListener('mouseup', this._handleMouseUpOrLeave);
+    this._nativeElement.removeEventListener('mouseleave', this._handleMouseUpOrLeave);
+  };
+
+  constructor(_elementRef: ElementRef) {
+    this._nativeElement = _elementRef.nativeElement;
+  }
+
+  ngOnInit(): void {
+    this._nativeElement.addEventListener('mousedown', this._handleMouseDown);
+  }
+
+  ngOnDestroy(): void {
+    this._nativeElement.removeEventListener('mousedown', this._handleMouseDown);
   }
 }
