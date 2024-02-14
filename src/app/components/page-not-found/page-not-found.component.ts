f8d52097931f2dd82922bf9f43126296a8f2e2d7
Trung D. Ngo <gguy0406@gmail.com>
2024-02-14 08:43:02 +0000

    chore: fix

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { StickyNavComponent } from '../sticky-nav/sticky-nav.component';
 
 @Component({
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [],
+  imports: [StickyNavComponent],
 })
 export class PageNotFoundComponent {}
